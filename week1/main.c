792b45a49a16a839516235328ec5efacebb58d13
226081745-Nashipili <226081745@nust.na>
2026-08-20 09:35:26 +0200

    Week 1 Fist C program V2

--- week1/main.c
+++ week1/main.c
@@ -1,8 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    ///////////practical ex1
-    
-    ///////////practical ex2
+int main()
+{
+    char municipality[50];
+    char mayor[50];
+    int population;
+
+    /* Display system title */
+    printf("===============================================\n");
+    printf("   MUNICIPAL FINANCIAL MANAGEMENT SYSTEM\n");
+    printf("===============================================\n");
+
+    /* Welcome message */
+    printf("Welcome to Windhoek Municipality\n");
+
+    /* Get user information */
+    printf("Enter Municipality Name: ");
+    scanf(" %s", municipality);
+
+    printf("Enter Mayor's Name: ");
+    scanf(" %[^\n]", mayor);
+
+    printf("Enter Population: ");
+    scanf("%d", &population);
+
+    /* Display report */
+    printf("\n============================================\n");
+    printf("       MUNICIPAL INFORMATION REPORT\n");
+    printf("============================================\n");
+    printf("Municipality Name : %s\n", municipality);
+    printf("Mayor's Name      : %s\n", mayor);
+    printf("Population        : %d\n", population);
+    printf("============================================\n");
+
     return 0;
 }
